45982d3ea841f37fe06a928e4510e7603d144a2d
chairco <chairco@gmail.com>
2017-10-22 20:43:57 +0800

    Release hackmd notify alarm function

--- src/hackmds/tasks.py
+++ src/hackmds/tasks.py
@@ -9,11 +9,11 @@ import diffhtml
 from django.shortcuts import render
 from django.conf import settings
 from django.core.mail import EmailMessage
+from django.template import loader
 from django.contrib.auth.models import User
-
-from bs4 import BeautifulSoup, NavigableString, Tag
-
 from hackmds.models import Archive
+
+from bs4 import BeautifulSoup
 
 from concurrent import futures
 
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# for test
+# For test
 if not settings.configured:
     """setting the Django env()"""
     logger.debug('settings.configured: {0}'.format(settings.configured))
@@ -49,11 +49,11 @@ def send_mail(email_subject, email_body, to, cc=None, bcc=None, from_email=None)
         cc = []
     else:
         cc = cc
+        cc = list(set(i.lower() for i in cc)) # all email str lower()，remove replicate email
 
     # all email str lower()，remove replicate email
-    to = list(set(i.lower for i in to))
-    cc = list(set(i.lower for i in cc))
-
+    to = list(set(i.lower() for i in to))
+    
     email = EmailMessage(
         subject=email_subject,
         body=email_body,
@@ -61,7 +61,7 @@ def send_mail(email_subject, email_body, to, cc=None, bcc=None, from_email=None)
         to=to,
         bcc=bcc,
         cc=cc,
-        headers={'Reply-To': from_email}
+        #headers={'Reply-To': from_email}
     )
     email.content_subtype = "html"
     return email.send(fail_silently=False)
@@ -74,8 +74,8 @@ def hackmd_notify_email(email_subject, result, *, cc=None):
         ) + 'hackmds_notify.html'
     )
     email_body = t.render({'result': result})
-    email_to = [u.email for u in user.objects.all()]
-    from_email = ['test@gmail.com']  # 暫時先放 test@gmail.com
+    email_to = [u.email for u in User.objects.all()]
+    from_email = 'chairco@gmail.com'  # 暫時先放 test@gmail.com
     return send_mail(
         email_subject=email_subject,
         email_body=email_body,
@@ -84,7 +84,7 @@ def hackmd_notify_email(email_subject, result, *, cc=None):
     )
 
 
-def hackmd_ck_task(url='https://hackmd.io/s/ByIn4AYaZ'):
+def hackmd_ck_task(url):
     """@bref open requests to parse information from hackmd.io
     """
     url = url.split('#')[0]  # get the url none anchor
@@ -96,7 +96,7 @@ def hackmd_ck_task(url='https://hackmd.io/s/ByIn4AYaZ'):
     content = soup.find(
         'div', {'id': 'doc', 'class': 'container markdown-body'})
     content = content.string
-    
+
     """@bref find keywords to generate notification
     """
     if len(Archive.objects.filter(url=url)):
@@ -106,22 +106,20 @@ def hackmd_ck_task(url='https://hackmd.io/s/ByIn4AYaZ'):
             diffhtml.ndiff(compare.content.splitlines(),
                            content.splitlines(), cutoff=cutoff)
         )  # default cutoff = 0.6
-        diff_count = str(result).count('<int>')
+        diff_count = str(result).count('<ins>')
         print('上一次內容差異數: {}'.format(diff_count))
-        
+
         if diff_count > 0:
             Archive.objects.filter(url=url).update(content=content)
 
-        #if diff_count > 5:
-        #    # notify by django email alarm by threading
-        #    with futures.ThreadPoolExecutor(workers=1) as execute:
-        #        res = execute.submit(hackmd_notify_email, url, result)
-        #    return res
+        if diff_count > 5:
+           # notify by django email alarm by threading
+           with futures.ThreadPoolExecutor(max_workers=1) as execute:
+               res = execute.submit(hackmd_notify_email, url, result)
+           return res
+           #hackmd_notify_email(email_subject='test', result=result)
         return
     else:
         print('第一次新增')
         Archive.objects.create(url=url, content=content)
     return
-
-if __name__ == '__main__':
-    hackmd_ck_task()
